c72c30a422e08808fa03aae314687bf421201d18
Mohd Arham <mdarham2405@gmail.com>
2025-10-26 22:09:56 +0530

    added basic gradient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="ADVAIT">
-      <h1>ADVAIT</h1>
-    </div>
+    <div className="background">
+        <div className="ADVAIT">
+          <h1>ADVAIT</h1>
+        </div>
+      <div className="glow-gradient">
+      </div>
+  </div>
   );
 }
