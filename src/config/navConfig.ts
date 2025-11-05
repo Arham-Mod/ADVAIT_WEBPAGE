54bdcdf27889928b43c683a32f74ffcb93bfba13
Mohd Arham <mdarham2405@gmail.com>
2025-11-05 17:42:43 +0530

    coustomize cards

--- src/config/navConfig.ts
+++ src/config/navConfig.ts
@@ -2,41 +2,41 @@ import type { CardNavItem } from '../components/CardNav'
 
 export const navItems: CardNavItem[] = [
   {
-    label: "Products",
+    label: "Events",
     bgColor: "#0d0716",
     textColor: "#ffffff",
     links: [
-      { label: "Feature 1", href: "#", ariaLabel: "Feature 1" },
-      { label: "Feature 2", href: "#", ariaLabel: "Feature 2" },
-      { label: "Feature 3", href: "#", ariaLabel: "Feature 3" },
+      { label: "Event 1", href: "#", ariaLabel: "Event 1" },
+      { label: "Event 2", href: "#", ariaLabel: "Event 2" },
+      { label: "Event 3", href: "#", ariaLabel: "Event 3" },
+    ]
+  },
+  {
+    label: "People",
+    bgColor: "#170d27",
+    textColor: "#ffffff",
+    links: [
+      { label: "Wall of Builders", href: "#", ariaLabel: "Wall of Builders" },
+      { label: "Hall of Flames", href: "#", ariaLabel: "Hall of Flames" },
+      { label: "Our Projects", href: "#", ariaLabel: "Our Projects" },
     ]
   },
   {
     label: "Services",
-    bgColor: "#170d27",
-    textColor: "#ffffff",
-    links: [
-      { label: "Service 1", href: "#", ariaLabel: "Service 1" },
-      { label: "Service 2", href: "#", ariaLabel: "Service 2" },
-      { label: "Service 3", href: "#", ariaLabel: "Service 3" },
-    ]
-  },
-  {
-    label: "About",
     bgColor: "#271e37",
     textColor: "#ffffff",
     links: [
       { label: "Our Story", href: "#", ariaLabel: "Our Story" },
-      { label: "Team", href: "#", ariaLabel: "Team" },
-      { label: "Contact", href: "#", ariaLabel: "Contact" },
+      { label: "Social Networks", href: "#", ariaLabel: "Social Networks" },
+      { label: "Newsletter", href: "#", ariaLabel: "Newsletter" },
     ]
   }
 ];
 
 export const navConfig = {
-  logo: "/next.svg",
+  logo: "/advait-logo.png",
   logoAlt: "ADVAIT Logo",
-  baseColor: "rgba(255, 255, 255, 0.1)",
+  baseColor: "rgba(249, 249, 249, 0.1)",
   menuColor: "#ffffff",
   buttonBgColor: "#ffffff",
   buttonTextColor: "#000000"
